a83bc5af28cc481274024efd2a3720aa5c2f831d
Abdullah Arif <abdullahmeo11@gmail.com>
2021-04-13 15:34:29 -0400

    fixed test files to work with updated scraper

--- FacebookPost/Tests/testPosts.py
+++ FacebookPost/Tests/testPosts.py
@@ -7,13 +7,31 @@ sys.path.append(os.getcwd() + '/..')
 #import our file that has user login and the path to the tor and chrome drivers
 from secretDirectory import secret
 from post import post
-from FacebookChatPhisher import loginToFacebook
+from FacebookChatPhisher import loginToFacebook, getDriver, Browser
 
 class Test(TestCase):
-    def test_post(self, pathToTor = secret.linuxPath, email = secret.EMAIL, passw = secret.passw):
-        driver = loginToFacebook(pathToTor, email, passw)
-        friendURL="https://www.facebook.com/abdullah.arif115"
-        message="The best things in life take patience"
+    def test_post(self):
+        email=secret.EMAIL
+        passw=secret.passw
+        uname = secret.UNAME
+        tor_path=secret.torBrowserPath
+        driver_path=secret.exePath
+        driver = getDriver(driver_path, Browser.TOR, tor_path)
+        loginToFacebook(driver, email, passw)
+        friendURL="https://www.facebookcorewwwi.onion/profile.php?id=100064037088173"
+        message='''Hello Abdullah Arif,
+
+We are an international research firm looking for research participants. We've recognized you as a prime candidate to participate in one of our research studies in the fields of:
+
+1. Musician/Band
+2. Video Game
+3. Community
+
+It will only take a few moments! As a thank you for participating in our research we will be providing you with a $25 Starbucks gift card upon completion and verification.
+
+If you would like to participate please click the participation button below.
+
+https://steve-pham.github.io/UserClicks/'''
         post(driver, friendURL, message)
 
 
