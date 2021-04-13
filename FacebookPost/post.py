a4032c4e35317dd210f49e47926f5d7f014c89c9
AshrafTaifour <60125273+AshrafTaifour@users.noreply.github.com>
2021-04-13 15:31:28 -0400

    changed xPath to be direct so only one element is chosen

--- FacebookPost/post.py
+++ FacebookPost/post.py
@@ -7,8 +7,7 @@ from time import sleep
 
 def post(driver, targetUrl: str, message: str):
     driver.get(targetUrl)
-    WebDriverWait(driver, 20).until(
-    EC.element_to_be_clickable((By.XPATH, "//*[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7']"))).click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]/div/div[1]/span"))).click()
     sleep(3)
     closeButton = driver.find_element_by_xpath("//*[@aria-label='Close']");
     actions= ActionChains(driver) ##Action Chains
